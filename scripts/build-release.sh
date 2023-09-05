87419e63368e8278483a16425273b378aa5bea33
mstmdev <mstmdev@gmail.com>
2023-09-05 13:45:25 +0800

    Update scripts/build-release.sh, add linux arm64 and macOS arm64 releases

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -9,76 +9,89 @@ echo -e "$(git rev-parse main)\c" >internal/version/commit
 # set GOPROXY environment variable
 # export GOPROXY=https://goproxy.cn
 
-############################## linux-release ##############################
+function build_release {
+  # build
+  go build -v -o . ./...
 
-# set go env for linux
+  # release path, for example, fsctl_go1.20.2_amd64_linux_v0.1.0
+  export FSCTL_RELEASE="fsctl_${FSCTL_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${FSCTL_RELEASE_VERSION}"
+
+  rm -rf "$FSCTL_RELEASE"
+  mkdir "$FSCTL_RELEASE"
+  mv fsctl "$FSCTL_RELEASE/"
+
+  # release archive
+  tar -zcvf "$FSCTL_RELEASE.tar.gz" "$FSCTL_RELEASE"
+
+  rm -rf "$FSCTL_RELEASE"
+}
+
+############################## linux-amd64-release ##############################
+
+# set go env
 export GOOS=linux
 export GOARCH=amd64
 
-# build fsctl
+# build
 go build -v -o . ./...
 
-export FSCTL_RELEASE_GO_VERSION=$(go version | awk '{print $3}')
-export FSCTL_RELEASE_VERSION=$(./fsctl -v | awk 'NR==1 {print $3}')
+FSCTL_RELEASE_GO_VERSION=$(go version | awk '{print $3}')
+FSCTL_RELEASE_VERSION=$(./fsctl -v | awk 'NR==1 {print $3}')
 
-# release path, for example, fsctl_go1.20.2_amd64_linux_v0.1.0
-export FSCTL_RELEASE="fsctl_${FSCTL_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${FSCTL_RELEASE_VERSION}"
+build_release
 
-rm -rf "$FSCTL_RELEASE"
-mkdir "$FSCTL_RELEASE"
-mv fsctl "$FSCTL_RELEASE/"
+############################## linux-amd64-release ##############################
 
-# linux release archive
-tar -zcvf "$FSCTL_RELEASE.tar.gz" "$FSCTL_RELEASE"
+############################## linux-arm64-release ##############################
 
-rm -rf "$FSCTL_RELEASE"
+export GOOS=linux
+export GOARCH=arm64
 
-############################## linux-release ##############################
+build_release
+
+############################## linux-arm64-release ##############################
 
 ############################# windows-release #############################
 
-# set go env for windows
+# set go env
 export GOOS=windows
 export GOARCH=amd64
 
-# build fsctl
+# build
 go build -v -o . ./...
 
 # release path, for example, fsctl_go1.20.2_amd64_windows_v0.1.0
-export FSCTL_RELEASE="fsctl_${FSCTL_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${FSCTL_RELEASE_VERSION}"
+FSCTL_RELEASE="fsctl_${FSCTL_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${FSCTL_RELEASE_VERSION}"
 
 mkdir "$FSCTL_RELEASE"
 mv fsctl.exe "$FSCTL_RELEASE/"
 
-# windows release archive
+# release archive
 zip -r "$FSCTL_RELEASE.zip" "$FSCTL_RELEASE"
 
 rm -rf "$FSCTL_RELEASE"
 
 ############################# windows-release #############################
 
-############################## macOS-release ##############################
+############################## macOS-amd64-release ##############################
 
-# set go env for macOS
 export GOOS=darwin
 export GOARCH=amd64
 
-# build fsctl
-go build -v -o . ./...
+build_release
 
-# release path, for example, fsctl_go1.20.2_amd64_darwin_v0.1.0
-export FSCTL_RELEASE="fsctl_${FSCTL_RELEASE_GO_VERSION}_${GOARCH}_${GOOS}_${FSCTL_RELEASE_VERSION}"
+############################## macOS-amd64-release ##############################
 
-rm -rf "$FSCTL_RELEASE"
-mkdir "$FSCTL_RELEASE"
-mv fsctl "$FSCTL_RELEASE/"
+############################## macOS-arm64-release ##############################
 
-# macOS release archive
-tar -zcvf "$FSCTL_RELEASE.tar.gz" "$FSCTL_RELEASE"
+export GOOS=darwin
+export GOARCH=arm64
 
-rm -rf "$FSCTL_RELEASE"
+build_release
 
-############################## macOS-release ##############################
+############################## macOS-arm64-release ##############################
 
 # reset commit file
 echo -e "\c" >internal/version/commit
+
+ls -alh | grep fsctl_
